94313a177756153b0e575f7d528fa00ce6f2089d
zip11 <zip11@sina.com>
2020-09-04 22:32:35 +0800

    Add files via upload

---
+++ bat批处理/病毒免疫/ipz病毒免疫.bat
@@ -0,0 +1,10 @@
+md C:\Windows\System32\ipz.exe
+attrib C:\Windows\System32\ipz.exe +r +a +h
+
+md C:\Windows\System32\ipz_db.bin
+attrib C:\Windows\System32\ipz_db.bin +r +a +h
+
+md C:\Windows\System32\ipz.tmp
+attrib C:\Windows\System32\ipz.tmp +r +a +h
+
+pause
---
+++ bat批处理/病毒免疫/viking病毒免疫.bat
@@ -0,0 +1,10 @@
+md C:\Windows\Logo1_.exe
+attrib C:\Windows\Logo1_.exe +r +a +h
+
+md C:\Windows\rundl132.exe
+attrib C:\Windows\rundl132.exe +r +a +h
+
+md C:\Windows\Dll.dll
+attrib C:\Windows\Dll.dll +r +a +h
+
+pause
