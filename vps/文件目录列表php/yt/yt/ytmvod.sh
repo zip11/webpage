6febac8e2ac79ae9111e315c3156a3f862a107b1
zip11 <zip11@sina.com>
2021-09-21 17:51:58 +0800

    Update ytmvod.sh

--- vps/文件目录列表php/yt/yt/ytmvod.sh
+++ vps/文件目录列表php/yt/yt/ytmvod.sh
@@ -7,11 +7,27 @@ files=$(ls *.mp4 2> /dev/null | wc -l)
 
 if [ "$files" != "0" ] ;
 then
-   echo 'part cun zai'
+   echo 'mp4 cun zai'
 else
    echo 'mp4 mei you'
    exit
 fi
+
+
+#vtt字幕
+for files in `ls |grep *.vtt`
+do
+    # 删除 
+    fname=${files/.zh-Hans/.mp4}
+
+
+    # 拼接成文件名
+    filename=$fname
+
+    # 更改文件名
+    mv $files $filename
+done
+
 
 #mei you part move *.mp4
 files=$(ls *.part 2> /dev/null | wc -l)
@@ -26,10 +42,11 @@ fi
 
 
 
+
 time2="ph$(date "+%Y%m%d%H%M%S").zip"
 
 cd /var/www/dl/wj/yt
-zip  -r -0  -P 123456  $time2  phvideo
+zip  -r -0  -P 741852  $time2  phvideo
 
 rclone move   /var/www/dl/wj/yt/$time2   onedrive:/video/phvideo
 
