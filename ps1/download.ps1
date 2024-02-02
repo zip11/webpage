f9fbb368c2b1e7fb0cfa1a313df58a66440b8429
zip11 <zip11@sina.com>
2024-02-02 21:49:51 +0800

    Create download.ps1

---
+++ ps1/download.ps1
@@ -0,0 +1,10 @@
+# 获取网址末尾的exe程序 名字
+$url = "https://cdn.aliyundrive.net/downloads/apps/desktop/aDrive-4.12.0.exe"
+
+# 获取url的最后一个元素，作为文件名
+$filename = $url.Split('/')[-1]
+
+# 下载文件到指定目录
+$destination = "d:\"
+Start-BitsTransfer -Source $url -Destination $destination$filename
+
