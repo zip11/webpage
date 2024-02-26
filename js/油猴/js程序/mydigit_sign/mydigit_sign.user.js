2227942648ccc689864355431be26bfb6ba281f2
zip11 <zip11@sina.com>
2024-02-26 23:36:56 +0800

    Update mydigit_sign.user.js

--- js/油猴/js程序/mydigit_sign/mydigit_sign.user.js
+++ js/油猴/js程序/mydigit_sign/mydigit_sign.user.js
@@ -10,8 +10,12 @@
 // @connect     www.mydigit.cn
 // ==/UserScript==
 
-(function() {
-    'use strict';
+
+'use strict';
+
+
+
+function read_button_link() {
 
     // 使用GM_xmlhttpRequest获取页面内容，并提取id为JD_sign的<a>标签的href属性
     // 替换为你想要请求的URL
@@ -37,7 +41,7 @@
                 console.log('找到链接：', link.href);
 
                 // 访问 签到网址
-                sign_page(link.href);
+                 sign_page(link.href);
 
             } else {
                 console.log('未找到ID为JD_sign的链接');
@@ -47,60 +51,92 @@
         onerror: function(error) {
             console.error('请求失败：', error);
         }
-    });
+    });   
+}
 
     // 访问 签到网址
-    function sign_page(link2) {
+function sign_page(link2) {
 
-        // 如果找到了链接，使用GM_xmlhttpRequest再次发送GET请求
-        GM_xmlhttpRequest({
+    // 如果找到了链接，使用GM_xmlhttpRequest再次发送GET请求
+    GM_xmlhttpRequest({
 
-            method: 'GET',
-            url: link2,
-            onload: function(getResponse) {
+        method: 'GET',
+        url: link2,
+        onload: function(getResponse) {
 
-                // 显示返回的内容
-                console.log('返回的内容：');
-                console.log(getResponse.responseText);
+            // 显示返回的内容
+            console.log('返回的内容：');
+            console.log(getResponse.responseText);
 
-                // 签到 成功 检测
-                let pageContent = getResponse.responseText;
-                // 签到 成功 字符串
-                let targetString = "CDATA[]";
+            // 签到 成功 检测
+            let pageContent = getResponse.responseText;
+            // 签到 成功 字符串
+            let targetString = "CDATA[]";
 
-                // 使用indexOf检查字符串是否存在
-                if (pageContent.indexOf(targetString) > -1) {
-                    console.log('签到成功：存在', targetString);
-                    msgk('签到成功：存在' + targetString)
-                } else {
-                    console.log('签到出错：不存在', targetString);
-                    msgk('签到出错：不存在' + targetString)
-                }
-
-            },
-            onerror: function(error) {
-                console.error('请求失败：', error);
+            // 使用indexOf检查字符串是否存在
+            if (pageContent.indexOf(targetString) > -1) {
+                console.log('签到成功：存在', targetString);
+                msgk('签到成功：存在' + targetString)
+                return "ok"
+            } else {
+                console.log('签到出错：不存在', targetString);
+                msgk('签到出错：不存在' + targetString)
+                return "error"
+                
             }
-        });        
 
-    }
+        },
+        onerror: function(error) {
+            console.error('请求失败：', error);
+        }
+    });        
+
+}
 
 
 
     // 消息框，自动关闭 函数
-    function msgk(text,sec) {
+function msgk(text,sec) {
 
-        // sec 如果没有输入参数，sec = 3
-        if (!sec) sec = 3;
+    // sec 如果没有输入参数，sec = 3
+    if (!sec) sec = 3;
 
-        GM_notification({
-            title: "Auto Close Notification",
-            text: text,
-            silent: true, // 是否在通知栏显示
-            timeout: sec*1000 // X秒后自动关闭
-        });
+    GM_notification({
+        title: "Auto Close Notification",
+        text: text,
+        silent: true, // 是否在通知栏显示
+        timeout: sec*1000 // X秒后自动关闭
+    });
+
+}
+
+
+
+// main
+return new Promise((resolve, reject) => {
     
+
+
+    try {
+
+        // let jg8 = await run1();
+
+        // if (jg8 == "油猴中文网bbs定时签到成功") {
+        
+        //     resolve("ok"+jg8);// 执行成功
+        
+        // } else {
+        
+        //     reject("error-exit"+jg8);// 执行失败,并返回错误原因
+        
+        // }        
+
+        read_button_link();
+        resolve("ok");// 执行成功
+
+    } catch (error) {
+
+        //GM_log(error);
+        reject(error);
     }
-
-
-})();
+});
